8de999f1e2558016a3c48f7af6e28b887dded3df
Castor Dunkels <castordnk@gmail.com>
2021-10-13 12:48:11 +0200

    hello

---
+++ FRÅGE_SPORT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FRÅGE_SPORT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
