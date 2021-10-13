900fc3d99c1f19c2093bf773b5ba14ad2fe56b1b
Castor Dunkels <castordnk@gmail.com>
2021-10-13 13:30:51 +0200

    frågor

--- FRÅGE_SPORT/Program.cs
+++ FRÅGE_SPORT/Program.cs
@@ -1,1 +1,42 @@
 ﻿using System;
+
+
+
+Console.WriteLine("Question one:");
+Console.WriteLine("What is the name of the director who made Dark Souls?");
+Console.ReadLine();
+
+
+Console.WriteLine("Question two:");
+Console.WriteLine("Which of the Dark Souls games is least liked?");
+Console.ReadLine();
+
+Console.WriteLine("Question three:");
+Console.WriteLine("Who was it that started the age of fire?");
+Console.ReadLine();
+
+Console.WriteLine("Question four:");
+Console.WriteLine("What is the name of the blacksmith that you meet in both Dark Souls 1 and 3?");
+Console.ReadLine();
+
+
+Console.WriteLine("Question five:");
+Console.WriteLine("What covenant does Solaire of Astora belong to?");
+Console.ReadLine();
+
+Console.WriteLine("Question six:");
+Console.WriteLine("Who is a traitor to their own race?");
+Console.ReadLine();
+
+Console.WriteLine("Question seven:");
+Console.WriteLine("Where does Dark Souls 1 take place?");
+
+Console.WriteLine("Question eight:");
+Console.WriteLine("What is the name of the city Gwyn founded?");
+Console.ReadLine();
+
+Console.WriteLine("Question nine");
+Console.WriteLine("What is your player character called in Dark Souls 3?");
+Console.ReadLine();
+
+
