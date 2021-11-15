3b2c86626548354cc78594abe7cd3902310ffc38
Castor Dunkels <castordnk@gmail.com>
2021-11-15 20:05:55 +0100

    method making

--- FRÅGE_SPORT/Program.cs
+++ FRÅGE_SPORT/Program.cs
@@ -1,143 +1,89 @@
 ﻿using System;
 int points = 0;
-int played = 0;
-void clear(){       //denna kod är en metod som, när den blir kallad så tar den bort all text som var på skärmen så den nya texten kommer att komma länst 
+string questionYN(string question){
+
+    while(true){
+        Console.WriteLine(question);
+        Console.WriteLine("Yes or No");
+        string playedornot = Console.ReadLine();
+        playedornot = playedornot.ToLower();
+        if(playedornot == "no" || playedornot == "n"){
+            return "no";
+        }
+        else if(playedornot == "yes" || playedornot == "y"){
+            return "yes";
+        }
+    }
+}
+
+string questionABC(string questionName, string questionabc, string answerA, string answerB, string answerC){
     Console.Clear();
-}
-
-Boolean repeatplayed = true;
-while(repeatplayed){
-    Console.WriteLine("Have you finished any of the Dark Souls games?");
-    Console.WriteLine("Yes or No");
-    string playedornot = Console.ReadLine();
-    playedornot = playedornot.ToLower();
-    if(playedornot == "yes" || playedornot == "no" || playedornot == "y" || playedornot == "n"){
-        repeatplayed = false;
-    }   
-    if(playedornot == "no" || playedornot == "n"){
-        played = 0;
-    }
-    else(playedornot == "yes" || playedornot == "y"){
-        played ++;
+    while(true){
+        Console.WriteLine(questionName);
+        Console.WriteLine(questionabc);
+        Console.WriteLine(answerA);
+        Console.WriteLine(answerB);
+        Console.WriteLine(answerC);
+        string answer = Console.ReadLine();
+        answer = answer.ToLower();
+        if(answer == "a" || answer == "b" || answer == "c"){
+            return answer;
+        }
     }
 }
 
-clear();
+questionYN("Have you finished more than one of the Dark Souls games?");
 
 
-Boolean repeat = true;
-while(repeat){
-    Console.WriteLine("Question one:");
-    Console.WriteLine("What is the name of the director who made Dark Souls?");
-    Console.WriteLine("A: Hidetaka Miyazaki");
-    Console.WriteLine("B: Hideo Kojima");
-    Console.WriteLine("C: Masahiro Sakurai");
-    string one = Console.ReadLine();
-    one = one.ToLower();
-    if(one == "a" || one == "b" || one == "c" ){
-        repeat = false;
-    }
+
+string answer;
+answer = questionABC("Question one:", "What is the name of the director who made Dark Souls?", 
+"A: Hidetaka Miyazaki", "B: Hideo Kojima", "C: Masahiro Sakurai"); 
+if(answer == "a"){
+    points ++;
 }
 
-clear();
 
-Boolean repeat2 = true;
-while(repeat2){
-    Console.WriteLine("Question two:");
-    Console.WriteLine("What is your player character called in Dark Souls 3?");
-    Console.WriteLine("A: Chosen undead");
-    Console.WriteLine("B: Bearer of the curse");
-    Console.WriteLine("C: Ashen one");
-    string two = Console.ReadLine();    
-    two = two.ToLower();    
-    if(two == "a" || two == "b" || two == "c" ){
-        repeat2 = false;
-    }
+
+answer = questionABC("Question two:", "What is your player character called in Dark Souls 3?", 
+"A: Chosen undead", "B: Bearer of the curse", "C: Ashen one");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
 
-Boolean repeat3 = true;
-while(repeat3){
-    Console.WriteLine("Question three:");
-    Console.WriteLine("Who was it that started the age of fire?");
-    Console.WriteLine("A: King Vendrick");
-    Console.WriteLine("B: Gwyn lord of cinders");
-    Console.WriteLine("C: Demon of hatred");
-    string three = Console.ReadLine();
-    three = three.ToLower();
-    if(three == "a" || three == "b" || three == "c" ){
-        repeat3 = false;
-    }
+answer = questionABC("Question three:", "Who was it that started the age of fire?", 
+"A: King Vendrick", "B: Gwyn lord of cinders", "C: Demon of hatred");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
-
-Boolean repeat4 = true;
-while(repeat4){
-    Console.WriteLine("Question four:");
-    Console.WriteLine("What is the name of the blacksmith that you meet in both Dark Souls 1 and 3?");
-    Console.WriteLine("A: Andre");
-    Console.WriteLine("B: Patches");
-    Console.WriteLine("C: Siegward");
-    string four = Console.ReadLine();
-    four = four.ToLower();
-    if(four == "a" || four == "b" || four == "c" ){
-        repeat4 = false;
-    }
+answer = questionABC("Question four:", "What is the name of the blacksmith that you meet in both Dark Souls 1 and 3?",
+"A: Andre", "B: Patches", "C: Siegward");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
-
-Boolean repeat5 = true;
-while(repeat5){
-    Console.WriteLine("Question five:");
-    Console.WriteLine("What covenant does Solaire of Astora belong to?");
-    Console.WriteLine("A: Blades of the Darkmoon");
-    Console.WriteLine("B: Warriors of sunlight");
-    Console.WriteLine("C: Spears of the church");
-    string five = Console.ReadLine();
-    five = five.ToLower();
-    if(five == "a" || five == "b" || five == "c" ){
-        repeat5 = false;
-    }
+answer = questionABC("Question five:", "What covenant does Solaire of Astora belong to?",
+"A: Blades of the Darkmoon", "B: Warriors of sunlight", "C: Spears of the church");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
-
-Boolean repeat6 = true;
-while(repeat6){
-    Console.WriteLine("Question six:");
-    Console.WriteLine("Who is a traitor to their own race?");
-    Console.WriteLine("A: Darkstalker Kaath");
-    Console.WriteLine("B: Annalise queen of the vilebloods");
-    Console.WriteLine("C: Seath the scaleless");
-    string six = Console.ReadLine();
-    six = six.ToLower();
-    if(six == "a" || six == "b" || six == "c" ){
-        repeat6 = false;
-    }
+answer = questionABC("Question six:", "Who is a traitor to their own race?",
+"A: Darkstalker Kaath", "B: Annalise queen of the vilebloods", "C: Seath the scaleless");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
-
-Boolean repeat7 = true;
-while(repeat7){
-    Console.WriteLine("Question seven:");
-    Console.WriteLine("Where does Dark Souls 1 take place?");
-    Console.WriteLine("A: Lothric");
-    Console.WriteLine("B: Lordran");
-    Console.WriteLine("C: Yharnam");
-    string seven = Console.ReadLine();
-    seven = seven.ToLower();
-    if(seven == "a" || seven == "b" || seven == "c" ){
-        repeat7 = false;
-    }
+answer = questionABC("Question seven:", "Where does Dark Souls 1 take place?",
+"A: Lothric", "B: Lordran", "C: Yharnam");
+if(answer == "c"){
+    points ++;    
 }
 
-clear();
+//if(questionYN == "yes" && points =< 2){
+//    Console.WriteLine("");
+//}
 
-if(played == 0 && points = 4){
-Console.WriteLine("Congrats you got ");
 Console.ReadLine();
-}
